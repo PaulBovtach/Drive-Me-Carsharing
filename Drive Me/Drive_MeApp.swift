08ba06173213bfcebb6de88900bbd1242c21be98
Paul Bovtach <natanbov@gmai.com>
2026-03-24 00:56:08 +0200

    Initial Commit

---
+++ Drive Me/Drive_MeApp.swift
@@ -0,0 +1,17 @@
+//
+//  Drive_MeApp.swift
+//  Drive Me
+//
+//  Created by Paul Bovtach on 24.03.2026.
+//
+
+import SwiftUI
+
+@main
+struct Drive_MeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

da59c0118dd1dd6f2710c1d0040229e37d4763a9
Paul Bovtach <natanbov@gmai.com>
2026-05-18 15:05:06 +0300

    added Secrets_Template

---
+++ Drive Me/Secrets_Template.swift
@@ -0,0 +1,15 @@
+//
+//  Secrets-Templates.swift
+//  Drive Me
+//
+//  Created by Paul Bovtach on 18.05.2026.
+//
+
+import Foundation
+
+//rename to 'Secrets'
+enum Secrets_Template {
+    
+    static let supabaseURL = "YOUR_SUPABASE_URL_HERE"
+    static let supabaseKey = "YOUR_SUPABASE_ANON_KEY_HERE"
+}
